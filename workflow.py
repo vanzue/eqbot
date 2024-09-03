10c3118279152014ae94e437f652d2dd684007e6
MorganeLu <lkx010215@126.com>
2024-09-03 09:50:50 +0800

    update workflow

--- workflow.py
+++ workflow.py
@@ -16,6 +16,7 @@ async def create_personal_info_endpoint(name: str, tag: str, tag_description: st
     db_personal_info = crud.create_personal_info(db, personal_info_data)
     return db_personal_info
 
+
 # create a new eq score report
 @router.post("/create_eq_scores")
 async def create_eqscore_endpoint(person_id: int, scores_details:dict, job_id: str, db: Session = Depends(database.get_db)):
@@ -39,19 +40,17 @@ async def create_eqscore_endpoint(person_id: int, scores_details:dict, job_id: s
     db_eq_score = crud.create_eq_score(db, eq_score_data)
     return db_eq_score
 
+
 # signup as a new user and get the EQ Score Report
 @router.post("/create_profile")
 async def create_profile(request: schemas.CreateUserRequest, background_tasks: BackgroundTasks, db: Session = Depends(database.get_db)):
     # Receive all the info from frontend
     # personal info
     username = request.info.username
-    # username = "Jay Park"
     
     # preference
     gender = request.preference.gender
     issues = request.preference.issues
-    # gender = "男"
-    # issues = ["不太擅长回复消息"]
 
     concerns = ", ".join(issues)
 
@@ -60,55 +59,30 @@ async def create_profile(request: schemas.CreateUserRequest, background_tasks: B
     answer2 = request.test.answer2
     answer3 = request.test.answer3
     answer4 = request.test.answer4
-    # answer1 = "等待领导决定"
-    # answer2 = "不理他"
-    # answer3 = "那我喝吧"
-    # answer4 = "帮客户清理并解释项目情况"
 
     # Send necessary info to llm agent and receive response from it
-    # TBD: subsitute by real test answers
     user_info = "该用户是一名" + gender + "性,ta在生活中经常受到" + concerns + "的困扰" \
                 + ".ta会在开会讨论遇到两个同事意见不合并且其中一个同事小王情绪很激动的时候，" + answer1 + "。" \
                 + "在饭局上，老板和ta开了一些不合适的玩笑，让ta感到非常不适，ta最有可能会" + answer2 + "。" \
                 + "在酒局上，重要客户说：今天ta不喝酒就是不给客户面子，ta最有可能用" + answer3 + "这句话婉拒。" \
                 + "在商务饭局上，客户说着对项目情况的担忧，同事正好把酒洒在客户身上，ta最有可能会说 “" + answer4 + "”。" 
-    # user_info = "该用户是一名女性，她会在开会讨论遇到两个同事意见不合并且其中一个情绪很激动的时候，冷静分析双方意见和优缺点"
 
     tags = ["超绝顿感力", "情绪小火山", "职场隐士", "交流绝缘体", "交流绝缘体"]
     tag_description = "TBD"
     job_id = str(uuid.uuid4())
-    background_tasks.add_task(process_user_data, request, user_info, tags, tag_description, job_id, db)
-
-    # llm_response = eq_eval.request_LLM_response(user_info)
-    # eq_scores = eq_eval.retry_parse_LLMresponse(llm_response)
-    # print(eq_scores)
-
-    # # 整理数据，明确什么是tag，什么是tag description，,如何计算以及overall score是否是五项均分
-    # # TBD: tag_description
-    # tag_description = "TBD"
-    # scores = [eq_scores['dimension1_score'], eq_scores['dimension2_score'], eq_scores['dimension3_score'], eq_scores['dimension4_score'], eq_scores['dimension5_score']]
-    # # overall_score = helper.calculate_average(*scores)
-    # tags = ["超绝顿感力", "情绪小火山", "职场隐士", "交流绝缘体", "交流绝缘体"]
-    # tag_id = helper.min_score_index(scores)
-    # job_id = str(uuid.uuid4())
-
-    # # create a new user
-    # db_personal_info = await create_personal_info_endpoint(name=username, tag=tags[tag_id], tag_description=tag_description, job_id=job_id, db=db)
-    
-    # # create EQ score
-    # await create_eqscore_endpoint(person_id=db_personal_info.id, scores_details=eq_scores, job_id=job_id, db=db)
+    background_tasks.add_task(process_user_data, username, user_info, tags, tag_description, job_id, db)
 
     return {"job_id": job_id}
 
-async def process_user_data(request, user_info, tags, tag_description, job_id, db):
-    # llm_response = eq_eval.request_LLM_response(user_info)
-    # eq_scores = eq_eval.retry_parse_LLMresponse(llm_response)
+
+async def process_user_data(username, user_info, tags, tag_description, job_id, db):
     eq_scores = eq_eval.retry_parse_LLMresponse(user_info)
     tag_id = helper.min_score_index([eq_scores['dimension1_score'], eq_scores['dimension2_score'], eq_scores['dimension3_score'], eq_scores['dimension4_score'], eq_scores['dimension5_score']])
     
     # create a new user and EQ score
-    db_personal_info = await create_personal_info_endpoint(name=request.info.username, tag=tags[tag_id], tag_description=tag_description, job_id=job_id, db=db)
+    db_personal_info = await create_personal_info_endpoint(name=username, tag=tags[tag_id], tag_description=tag_description, job_id=job_id, db=db)
     await create_eqscore_endpoint(person_id=db_personal_info.id, scores_details=eq_scores, job_id=job_id, db=db)
+
 
 @router.get("/get_profile/{job_id}")
 async def get_profile(request: Request, job_id: str, db: Session = Depends(database.get_db)):
@@ -152,7 +126,3 @@ def loginin_user(request: Request, name: str, db: Session = Depends(database.get
     personal_id = crud.get_personal_id_by_name(db, name=name)
 
     return personal_id
-
-
-# if __name__ == "__main__":
-#     create_user()
