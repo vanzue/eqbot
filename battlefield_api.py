a7df33ac8a821404b439b1378f31165d9e5e4487
MorganeLu <lkx010215@126.com>
2024-10-17 10:00:11 +0800

    update battlefield

--- battlefield_api.py
+++ battlefield_api.py
@@ -6,43 +6,27 @@ from sqlalchemy.orm import Session
 import data_types
 from database import database, crud, schemas
 
-from llm.chat_battlefield import request_LLM_response, chat_eval
+from llm.chat_battlefield import request_LLM_response, chat_eval, request_LLM_response_v2
 
 router = APIRouter()
 
 
+@router.post("/chat/batttlefield/v2")
+def chat_battlefield(request: data_types.BattlefieldRequest, db: Session = Depends(database.get_db)):
+    personal_id = request.person_id
+    course_id = request.course_id
+    
+    matching_course = crud.get_course_by_coursid(db, course_id=course_id)
+    print(matching_course.prompt)
+
+    response = request_LLM_response_v2(json.loads(request.chat_content), matching_course.prompt)
+    return response
 
 @router.post("/chat/batttlefield")
 def chat_battlefield(request: data_types.BattlefieldRequest, db: Session = Depends(database.get_db)):
     personal_id = request.person_id
     course_id = request.course_id
 
-    # eq_scores = crud.get_eq_scores_by_person_id(db, person_id=personal_id)
-
-    # if not eq_scores:
-    #     print("no eq scores")
-    #     lowest_dimension = 1
-    # else:
-    #     scores = {
-    #         "dimension1": eq_scores.dimension1_score,
-    #         "dimension2": eq_scores.dimension2_score,
-    #         "dimension3": eq_scores.dimension3_score,
-    #         "dimension4": eq_scores.dimension4_score,
-    #         "dimension5": eq_scores.dimension5_score
-    #     }
-        
-    #     # 找到最低分的维度
-    #     lowest_dimension = min(scores, key=scores.get)
-        
-    # eq_dimensions = ["情绪侦查力", "情绪掌控力", "人际平衡术", "沟通表达力", "社交得体度"]
-    # eq_type = eq_dimensions[lowest_dimension]
-
-    # person_course = crud.get_courses_by_person_id(db, person_id=personal_id)
-    # if not person_course:
-    #     matching_courses = crud.get_course_by_course_type_and_level(db, course_type=eq_type, course_level=1)
-    
-    # # TBD：根据个人进度，匹配的课程
-    # matching_courses = crud.get_course_by_course_type_and_level(db, course_type=eq_type, course_level=1)
     matching_course = crud.get_course_by_coursid(db, course_id=course_id)
     print(matching_course.prompt)
 
