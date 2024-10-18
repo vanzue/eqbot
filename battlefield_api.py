c1d44f51209e459b1156e27025317b57fda83baf
MorganeLu <lkx010215@126.com>
2024-10-18 11:30:12 +0800

    update get battlefield

--- battlefield_api.py
+++ battlefield_api.py
@@ -109,8 +109,10 @@ def get_battlefield(person_id: int, db: Session = Depends(database.get_db)):
     # eq_type = eq_dimensions[lowest_dimension]
     eq_type = "情绪掌控力"
 
-    person_course = crud.get_coursesperson_by_person_id(db, person_id=person_id)
+    person_course = crud.get_coursesperson_by_person_id_all(db, person_id=person_id)
     if not person_course:
-        person_course = crud.get_course_by_course_type_and_level(db, course_type=eq_type, course_level=1)
-    
-    return {"course": person_course}
+        new_course = crud.get_course_by_course_type_and_level(db, course_type=eq_type, course_level=1)
+        return {"courses": [new_course.id]}
+    else:
+        return {"courses": person_course}
+    # return {"course": person_course}
