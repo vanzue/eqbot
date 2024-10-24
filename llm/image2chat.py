b7afe867af10f442d3ccddd6d624b945568fc1af
MorganeLu <lkx010215@126.com>
2024-10-24 18:24:17 +0800

    image 2 chat

--- llm/image2chat.py
+++ llm/image2chat.py
@@ -3,16 +3,86 @@ import requests
 import base64
 import easyocr
 from langchain_core.prompts import ChatPromptTemplate
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from openai import AzureOpenAI
 
-from llm.keyless_setup import creat_llm
+from keyless_setup import creat_llm
 
 
-def image2text(image_path):
-    IMAGE_PATH = image_path
-    # encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')
+# def image2text(image_path):
+#     IMAGE_PATH = image_path
+#     # encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')
 
-    reader = easyocr.Reader(['ch_sim', 'en'])  # 支持中文和英文
-    encoded_image = reader.readtext(IMAGE_PATH)
+#     reader = easyocr.Reader(['ch_sim', 'en'])  # 支持中文和英文
+#     encoded_image = reader.readtext(IMAGE_PATH)
+
+#     system_prompt = """
+#         **Task Description**
+#         - You're an image recognition expert. You will receive a screenshot of my chat with someone else. You need to extract our conversation.
+
+#         **Main Details**
+#         - Message has to inlcude:
+#             1. message sender {{user}}
+#             2. message content {{message}}
+
+#         **Extra Instructions**
+#         The sender of the message on the left is the peer, and the sender of the message on the right is yourself. If the person's name is missing, use "them" instead.
+        
+#         **Standard Output Format**
+#         [
+#             {{"user": "me", "message": [message content]}}, 
+#             {{"user": [peer name], "message": [message content]}}
+#         ]
+
+#         ** Extra Instructions **
+#         If the language used in image is chinese, then the output should be chinese.
+#         If the language used in image is english, then the output should be english.
+
+#     """
+
+#     user_prompt = """
+#                 **The follwoing is a screenshot of the chat history**
+
+#                 {encoded_image}
+#                 """
+
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             ('system', system_prompt),
+#             ('user', user_prompt),
+#         ]
+#     )
+    
+#     input_dict = {'encoded_image': encoded_image}
+
+#     llm = creat_llm()
+#     model = prompt | llm
+
+#     analysis_output = model.invoke(input_dict)
+#     output = analysis_output.content
+#     print(output)
+
+#     return output
+
+
+
+ENDPOINT = os.getenv('AZURE_OPENAI_ENDPOINT_LONG')
+AZURE_DEPLOYMENT = os.getenv('AZURE_DEPLOYMENT')
+
+def image2text(image_path):  
+    IMAGE_PATH = image_path     
+
+    token_provider = get_bearer_token_provider(
+        DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+    )
+
+    client = AzureOpenAI(
+        api_version="2024-03-01-preview",
+        azure_endpoint=ENDPOINT,
+        azure_ad_token_provider=token_provider,
+    )
+
+    encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')      
 
     system_prompt = """
         **Task Description**
@@ -35,32 +105,27 @@ def image2text(image_path):
         ** Extra Instructions **
         If the language used in image is chinese, then the output should be chinese.
         If the language used in image is english, then the output should be english.
-
     """
 
-    user_prompt = """
-                **The follwoing is a screenshot of the chat history**
 
-                {encoded_image}
-                """
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ('system', system_prompt),
-            ('user', user_prompt),
-        ]
+    response = client.chat.completions.create(
+        # For Azure OpenAI, the model parameter must be set to the deployment name
+        model=os.getenv("AZURE_DEPLOYMENT"),
+        temperature=0.7,
+        n=1,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": 
+                                    [{           
+                                        "type": "image_url",           
+                                        "image_url": {             
+                                            "url": f"data:image/jpeg;base64,{encoded_image}"           
+                                        }         
+                                    }]    
+            },
+        ],
     )
-    
-    input_dict = {'encoded_image': encoded_image}
-
-    llm = creat_llm()
-    model = prompt | llm
-
-    analysis_output = model.invoke(input_dict)
-    output = analysis_output.content
-    print(output)
-
-    return output
+    return response.choices[0].message.content
 
 
 if __name__ == "__main__":
