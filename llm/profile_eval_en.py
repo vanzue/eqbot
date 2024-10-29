121ad6e4ba25fe56b801398d8565fc0d7583a709
MorganeLu <lkx010215@126.com>
2024-10-29 17:54:38 +0800

    update prompt

--- llm/profile_eval_en.py
+++ llm/profile_eval_en.py
@@ -115,13 +115,13 @@ def request_LLM_response(scenario):
                             "Tips": "[Provide a short tip, 20 words or less]
                         }},
                         "7. Cultivation Suggestions": {{
-                            "Suggestions": "[Provide detailed analysis by including both pros and cons that show in senes and also consists content in scenes. Make some suggestions]"
+                            "Suggestions": "[Provide detailed analysis by including both pros and cons that show in senes and also consists content in scenes. Please make the suggestions mainly based on the cons!]"
                         }},
                         "8. Summary of Cultivation Suggestions": {{
                             "Summary of Suggestions": "[Summarize the suggestions based on cultivation suggestions into one sentence, like a title]"
                         }},
                         "9. Overall Suggestion": {{
-                            "Overall Suggestion": "[According to the user's situation, give a comprehensive evaluation and good wishes to the user]"
+                            "Overall Suggestion": "[According to the user's situation, give a comprehensive evaluation and good wishes to the user, realted to the user's cons]"
                         }}
                     }}
                     """
