eec76e57fc906164d04cb33dbbec6b23b1fc2686
MorganeLu <lkx010215@126.com>
2024-11-06 10:37:47 +0800

    update moments

--- network_api.py
+++ network_api.py
@@ -171,7 +171,7 @@ async def upload_image(file: UploadFile = File(...)):
 
 
 @router.post("/analyze/history")
-async def analyze_history_from_image(locale: str, user_id: int = Form(...), file: UploadFile = File(...), db: Session = Depends(database.get_db)):
+async def analyze_history_from_image(locale: str = "en", user_id: int = Form(...), file: UploadFile = File(...), db: Session = Depends(database.get_db)):
     chat_history_response = await upload_image(file)
     chat_history = chat_history_response["chat_history"]
     summary = chat_history_response["summary"]
